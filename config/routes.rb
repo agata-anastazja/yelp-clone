5a591f15a5f2b1aeba5762754b1a31df03038e65
pflarcology <rhrg500@gmail.com>
2017-01-10 16:46:16 +0000

    installs devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root "restaurants#index"
+
   get 'restaurants' => 'restaurants#index'
   resources :restaurants do
     resources :reviews
   end
+
 end
